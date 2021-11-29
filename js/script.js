e73842838ba9b53f2954912d53530927ed597894
adeola <durodolaadeola7@gmail.com>
2021-11-29 13:36:36 +0100

    output

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-function piglatin(userInput)
- if (vowels.indexOf(userInput[0])> -1) {
-    newStr = userInput +"acedu";
-    console.log(newStr)
-}else{
-    console.log(remove + "ayac")
-}
+
+
 
 
 
@@ -15,13 +10,32 @@ function piglatin(userInput)
 $(document).ready(function(){
     $(".form").submit(function(event){
         event.preventDefault()
-       
         let userInput =$("#input1").val();
+        userInput =userInput.toLowerCase()
+        //getting the first character and concatinating it to the back of my input
         let firstLetter = userInput.substring(0,1);
         let remove = userInput.slice(1)+firstLetter;
+
+
+        //getting the first two letters and concatinating it to the back of my input
+        let twoLetter = userInput.substring(0,2)
+        let remove2 = userInput.slice(2)+twoLetter
+
+
         let vowels = ['a', 'e', 'i', 'o', 'u'];
         let newStr= "";
-       
+        if (vowels.indexOf(userInput[0])> -1) {
+            newStr = userInput +"acedu";
+            console.log(newStr)
+        }
+        
+        else if(twoLetter==="qu"){
+                $("#output").text(remove2 + "ayac")
+        }
+        
+        else{
+            console.log(remove + "ayac")
+        }
 
     })
 })
